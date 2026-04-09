dff8b76e10dc00f31d9e7f4e5939b072033e26c2
Aram Kocharyan <aram.koch@gmail.com>
2026-04-09 21:16:10 +0400

    Fix the voter/candidate count

--- lib/elections-config.ts
+++ lib/elections-config.ts
@@ -4,8 +4,8 @@ export const CANDIDATE_FEE = 50000;
 export const VOTER_POINTS = 1000;
 export const CANDIDATE_POINTS = 3000;
 
-export const VOTER_GOAL = 1000;
-export const CANDIDATE_GOAL = 100;
+export const VOTER_GOAL = 500;
+export const CANDIDATE_GOAL = 50;
 
 export function formatAMD(amount: number) {
   return amount.toLocaleString("hy-AM") + " ֏";
